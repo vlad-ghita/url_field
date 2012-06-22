396ddd3314ed579641e627d9fea62e3baf189ced
Vlad Ghita <vlad_micutul@yahoo.com>
2012-06-22 09:49:00 +0300

    Added Romanian localisation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,32 @@
+<?php
+
+	$about = array(
+		'name' => 'New translation',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad.ghita@xandergroup.ro',
+			'website' => 'http://www.xanderadvertising.com'
+		),
+		'release-date' => '2012-06-22'
+	);
+
+	/**
+	 * Field: URL
+	 */
+	$dictionary = array(
+
+		// Missing
+
+		'Values' => 
+		'Valori',
+
+		'%s Internal' => 
+		'%s Intern',
+
+		'%s External' => 
+		'%s Extern',
+
+		'Invalid URI.' => 
+		'URI invalid',
+
+	);
